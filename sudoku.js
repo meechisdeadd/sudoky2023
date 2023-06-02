3f502e108dc525f035b1f52c2aa09d193ac50b82
Antonnoid <izhanton@gmail.com>
2023-06-02 15:59:58 +0300

    сделана функия проверки

--- sudoku.js
+++ sudoku.js
@@ -3,25 +3,46 @@
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-function solve(boardString) {
-}
+function solve(boardString) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
 function isSolved(board) {
-
+  if (board.flat().includes(0)) {
+    return false;
+  }
+  for (let i = 0; i < 9; i++) {
+    if (
+      new Set(board[i]).size !== 9 ||
+      new Set(board.map((row) => row[i])).size !== 9
+    ) {
+      return false;
+    }
+  }
+  // Проверка уникальности цифр в квадратах 3x3
+  for (let i = 0; i < 9; i += 3) {
+    for (let j = 0; j < 9; j += 3) {
+      let square = [];
+      for (let k = i; k < i + 3; k++) {
+        for (let l = j; l < j + 3; l++) {
+          square.push(board[k][l]);
+        }
+      }
+      if (new Set(square).size !== 9) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
-
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
