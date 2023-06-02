556d274b0a66912ea77f6b86c901bee4549cc6e3
meechisdeadd <ifuckyourgrandma@mail.ru>
2023-06-02 16:01:08 +0300

    Матрица-создание

--- runner.js
+++ runner.js
@@ -1,14 +1,11 @@
 // Подключить модуль работы с файловой системой.
-const fs = require('fs');
+const fs = require("fs");
 
 // Подключить функцию readAndSolve из соответствующего файла.
-const readAndSolve = require('./readAndSolve');
+const readAndSolve = require("./readAndSolve");
+const sudoku = createEmptyGrid();
+
 
 // Все судоку для решения доступны в файле puzzles.txt.
 // Прочесть файл puzzles.txt в кодировке 'utf-8' и передать его содержимое в функцию readAndSolve.
-fs.readFile(
-  './puzzles.txt',
-  'utf-8',
-  readAndSolve,
-);
-console.log(123);
+fs.readFile("./puzzles.txt", "utf-8", readAndSolve);
